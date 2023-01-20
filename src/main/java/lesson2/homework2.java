66ef2eec8cfa97ab92ad2dfa8c3b0ec7439c7aac
Sergei904025 <105377873+Sergei904025@users.noreply.github.com>
2023-01-20 15:57:33 +0600

    Homework 2

---
+++ src/main/java/lesson2/homework2.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class homework2 {
+    public class main2 {
+        public static void main(String[] args) {
+            System.out.println("my lesson1");
+        }
+    }
+}
